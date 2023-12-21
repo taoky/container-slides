78c235769cf5f0a84b80c4c74766255551e1daf7
taoky <me@taoky.moe>
2023-12-22 03:25:58 +0800

    Working hackergame composerized

--- hackergame/conf/settings/docker.py
+++ hackergame/conf/settings/docker.py
@@ -1,5 +1,10 @@
 from .hackergame import *
+import os
 
+# Official domain and localhost for local test
+ALLOWED_HOSTS = ["hack.lug.ustc.edu.cn", '.localhost', '127.0.0.1', '[::1]']
+# For local test
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -7,14 +12,15 @@ DATABASES = {
         'USER': 'hackergame',
         'CONN_MAX_AGE': 0,
         'ATOMIC_REQUESTS': True,
-        'HOST': 'pgbouncer',
+        'HOST': 'hackergame-pgbouncer',
         'PORT': 5432,
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
     },
 }
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': 'memcached:11211',
+        'LOCATION': 'hackergame-memcached:11211',
         'TIMEOUT': 3600,
         'KEY_PREFIX': 'hackergame',
     },
